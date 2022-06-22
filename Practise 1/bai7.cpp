cee358f415d5295b89bffa8f082ea88119d0d884
QuanMCPC <60545346+QuanMCPC@users.noreply.github.com>
2022-06-22 12:00:22 +0700

    Update some tsuff

--- Practise 1/bai7.cpp
+++ Practise 1/bai7.cpp
@@ -9,15 +9,16 @@ int main()
     if (delta < 0)
     {
         cout << "Phuong trinh vo nghiem";
-        return 0;
     }
     else if (delta == 0)
     {
         cout << "Phuong trinh co nghiem kep x1, x2: " << -b / (2 * a);
-        return 0;
     }
+    else
+    {
     cout << "Phuong trinh " << a << "x2 + " << b << "x + " << c << " = 0 co:\n";
     cout << " - x1 = " << (-b + sqrt(delta)) / (2 * a) << "\n";
     cout << " - x2 = " << (-b - sqrt(delta)) / (2 * a) << "\n";
+    }
     return 0;
 }
